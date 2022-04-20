68db79e5e3f10c41fec510e0254ec9a2f245d973
Tarush Goyal <tarush.g@commerceiq.ai>
2022-04-20 15:26:23 +0530

    initial commit

---
+++ complete/src/main/java/com/example/springboot/Multiply.java
@@ -0,0 +1,16 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Multiply {
+
+
+    @GetMapping("/calserv/multiply/{num1}/{num2}")
+    public long index(@PathVariable int num1, @PathVariable int num2 ) {
+        return num1*num2;
+    }
+
+}
